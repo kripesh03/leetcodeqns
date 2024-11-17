7758f49d8d758422ea314ff16a00d0176611782d
kripesh82 <pkripesh345@gmail.com>
2024-11-17 18:37:34 +0545

    first commit

---
+++ two_sums.py
@@ -0,0 +1,9 @@
+nums = [2,3,4]
+target = 6
+def twoSum(nums, target):
+    for i in range(len(nums)): 
+        for j in range(i + 1, len(nums)):  
+            if nums[i] + nums[j] == target: 
+                return [i, j]  
+            
+print(twoSum(nums,target))
